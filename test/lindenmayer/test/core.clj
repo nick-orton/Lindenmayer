488f6bd0b47ba89571ac018fc25857f92e9ff421
Nick Orton <orton.nick@gmail.com>
2011-07-24 20:44:44 -0400

    reformatting

--- test/lindenmayer/test/core.clj
+++ test/lindenmayer/test/core.clj
@@ -2,7 +2,11 @@
   (:use [lindenmayer.core])
   (:use [clojure.test]))
 
-(def undertest (l-sys {:a [:a :b :c] :b [:b :b] :c [:c :b :a]}))
+(def rules {:a [:a :b :c]
+            :b [:b :b] 
+            :c [:c :b :a]})
+
+(def undertest (l-sys rules ))
 
 (deftest expand
   (is (= [:a :b :c] (beta-subs undertest [:a] ))))
