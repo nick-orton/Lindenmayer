896be4f9dbe269e68d1c8e80243f09eb17debd3f
Nick Orton <orton.nick@gmail.com>
2011-07-24 21:02:38 -0400

    expand-n

--- src/lindenmayer/core.clj
+++ src/lindenmayer/core.clj
@@ -2,10 +2,17 @@
 
 (defprotocol Grammar
   (expand [this seed])
+  (expand-n [this seed n])
   )
 
 (defn l-sys
   [rules]
   (reify Grammar
-    (expand [this seed] (flatten (map rules seed))))
-  )
+    (expand [this seed] (flatten (map rules seed)))
+    (expand-n 
+      [this seed n]
+      (if (= n 1)
+          (expand this seed)
+          (expand-n this (expand this seed) (- n 1)) 
+      ))
+  ))
--- test/lindenmayer/test/core.clj
+++ test/lindenmayer/test/core.clj
@@ -11,3 +11,6 @@
 (deftest test-expand
   (is (= [:a :b :c ] (expand undertest [:a] )))
   (is (= [:a :b :c :b :b :c :b :a] (expand undertest (expand undertest [:a] )))))
+
+(deftest test-expand-n
+  (is (= [:a :b :c :b :b :c :b :a] (expand-n undertest [:a] 2))))
