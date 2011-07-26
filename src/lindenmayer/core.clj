7c222f06457b84a611af4f73f527f5b03780cb2e
Nick Orton <orton.nick@gmail.com>
2011-07-25 23:13:45 -0400

    renaming protocol

--- src/lindenmayer/core.clj
+++ src/lindenmayer/core.clj
@@ -2,7 +2,7 @@
   (use [clojure.set] ))
 
 
-(defprotocol Grammar
+(defprotocol LindenmayerSystem
   (expand [this seed])
   (expand-n [this seed n])
   )
@@ -17,7 +17,7 @@
   [rules]
   (if (not (validate rules))
       (throw (IllegalArgumentException.))
-      (reify Grammar
+      (reify LindenmayerSystem
         (expand [this seed] (flatten (map rules seed)))
         (expand-n 
           [this seed n]
